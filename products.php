fd1f54292e0f0e32b04fc5d67c951d9a53421be2
Gabs343 <gabriellopezp343@gmail.com>
2021-01-25 20:53:08 -0300

    archivo que muestra los  productos

---
+++ products.php
@@ -0,0 +1,80 @@
+<?php
+    require_once("head.php");
+?>
+
+<section class="container-fluid">
+    <div>
+        <ul>
+        </ul>
+
+        <ul>
+        </ul>
+    </div>
+
+    <div class="row ">
+        <div class="col index-product card">
+            <a href="">
+                <img src="img/auricular1.png" alt="First slide" class="w-100">    
+            </a>
+                <div class="card-body">
+                    <h5 class="etiqueta-nombre">Nombre</h5>
+                    <p class="etiqueta-precio">Precio</p>
+                    <span class="btn-shop"><a href=""><i class="fas fa-cart-plus"></i></a></span>
+                </div>
+        </div>
+        <div class="col index-product card">
+            <a href="">
+                <img src="img/auricular1.png" alt="First slide" class="w-100">    
+            </a>
+                <div class="card-body">
+                    <h5 class="etiqueta-nombre">Nombre</h5>
+                    <p class="etiqueta-precio">Precio</p>
+                    <span class="btn-shop"><a href=""><i class="fas fa-cart-plus"></i></a></span>
+                </div>
+        </div>
+        <div class="col index-product card">
+            <a href="">
+                <img src="img/auricular1.png" alt="First slide" class="w-100">    
+            </a>
+                <div class="card-body">
+                    <h5 class="etiqueta-nombre">Nombre</h5>
+                    <p class="etiqueta-precio">Precio</p>
+                    <span class="btn-shop"><a href=""><i class="fas fa-cart-plus"></i></a></span>
+                </div>
+        </div>
+        <div class="col index-product card">
+            <a href="">
+                <img src="img/auricular1.png" alt="First slide" class="w-100">    
+            </a>
+                <div class="card-body">
+                    <h5 class="etiqueta-nombre">Nombre</h5>
+                    <p class="etiqueta-precio">Precio</p>
+                    <span class="btn-shop"><a href=""><i class="fas fa-cart-plus"></i></a></span>
+                </div>
+        </div>
+        <div class="col index-product card">
+            <a href="">
+                <img src="img/auricular1.png" alt="First slide" class="w-100">    
+            </a>
+                <div class="card-body">
+                    <h5 class="etiqueta-nombre">Nombre</h5>
+                    <p class="etiqueta-precio">Precio</p>
+                    <span class="btn-shop"><a href=""><i class="fas fa-cart-plus"></i></a></span>
+                </div>
+        </div>
+        <div class="col index-product card">
+            <a href="">
+                <img src="img/auricular1.png" alt="First slide" class="w-100">    
+            </a>
+                <div class="card-body">
+                    <h5 class="etiqueta-nombre">Nombre</h5>
+                    <p class="etiqueta-precio">Precio</p>
+                    <span class="btn-shop"><a href=""><i class="fas fa-cart-plus"></i></a></span>
+                </div>
+        </div>
+    </div>
+</section>
+
+<?php 
+    require_once("footer.php");
+?>
