1c8d62b6c6e3f40a0a15fd51b9b0dbd26affa432
Gabs343 <gabriellopezp343@gmail.com>
2021-01-30 19:13:26 -0300

    matriz de productos y funcion

--- head.php
+++ head.php
@@ -1,4 +1,88 @@
 <?php
+
+    $a_productos = array(
+        1 => array(
+            "id_producto" => 1,
+            "imagen" => "img/auricular1.png",
+            "nombre" => "Memecoleous H112",
+            "descripcion" => "El Redragon Memecoleous ofrece un increíble nivel de sonido a gamers gracias a su sonido diseñado a medida.",
+            "precio" => 999,
+            "id_marca" => 1,
+            "id_categoria" => 1,
+            "nuevo" => false,
+            "destacado" => false,
+            "proximamente" => false,
+            "especificaciones" => array(
+                "Tipo de salida" => "Stereo",
+                "Tipo de copa" => "Circumaurales: Este tipo de auriculares se coloca rodeando completamente la oreja. Tacto suave y con aislamiento pasivo",
+                "Material Diadema" => "Metálica, flexible. Integrada a la estructura",
+                "Tipo de cable" => "2 m Engomado de alta resistencia.",
+                "Diametro del diafragma" => "50 mm con imanes de neodimio",
+                "Vibración" => "Si. Motor de vibración 30 mm",
+                "Peso" => "450 gr"
+            )
+        ),
+
+        2 => array(
+            "id_producto" => 2,
+            "imagen" => "img/teclado1.png",
+            "nombre" => "Kumara K552",
+            "descripcion" => "",
+            "precio" => 999,
+            "id_marca" => 1,
+            "id_categoria" => 1,
+            "nuevo" => false,
+            "destacado" => false,
+            "proximamente" => false,
+            "especificaciones" => array()
+        ),
+
+        3 => array(
+            "id_producto" => 3,
+            "imagen" => "img/mouse1.png",
+            "nombre" => "Deathadder V2",
+            "descripcion" => "",
+            "precio" => 999,
+            "id_marca" => 1,
+            "id_categoria" => 1,
+            "nuevo" => false,
+            "destacado" => false,
+            "proximamente" => false,
+            "especificaciones" => array()
+        ),
+
+        4 => array(
+            "id_producto" => 4,
+            "imagen" => "img/controller1.png",
+            "nombre" => "Joystick Xbox",
+            "descripcion" => "",
+            "precio" => 999,
+            "id_marca" => 1,
+            "id_categoria" => 1,
+            "nuevo" => false,
+            "destacado" => false,
+            "proximamente" => false,
+            "especificaciones" => array()
+        ),
+
+        5 => array(
+            "id_producto" => 5,
+            "imagen" => "img/teclado2.png",
+            "nombre" => "Prodigy G213",
+            "descripcion" => "",
+            "precio" => 999,
+            "id_marca" => 1,
+            "id_categoria" => 1,
+            "nuevo" => false,
+            "destacado" => false,
+            "proximamente" => false,
+            "especificaciones" => array()
+        )
+    );
+
+    $json_productos = json_encode($a_productos);
+    file_put_contents("productos.json", $json_productos);
+
     $items_navlist = array(
         1 => array(
             "archivo" => "index.php",
@@ -36,6 +120,19 @@
     function NavActive($itemNav){
         echo strpos($_SERVER["SCRIPT_NAME"], $itemNav) ? "active" : "";
     }
+
+    function Producto($id_producto, $a_productos){?>
+        <div class="col index-product card">
+            <a href="">
+                <img src="<?php echo $a_productos[$id_producto]["imagen"]; ?>" alt="First slide" class="w-100">    
+            </a>
+            <div class="card-body">
+                <h5 class="etiqueta-nombre"><?php echo $a_productos[$id_producto]["nombre"]; ?></h5>
+                <p class="etiqueta-precio"><?php echo "$ ", $a_productos[$id_producto]["precio"]; ?></p>
+                <span class="btn-shop"><a href=""><i class="fas fa-cart-plus"></i></a></span>
+            </div>
+        </div>
+    <?php }
 ?>
 
 
@@ -56,6 +153,10 @@
 
     <!--Iconos de Fontawesome -->
     <script src="https://kit.fontawesome.com/cedf025736.js" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!--<script src="jquery.js"></script>-->
+    
 </head>
 
 <body>
