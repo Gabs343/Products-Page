10e851305311113ed2525b05d8d7afa0699710e4
Gabs343 <gabriellopezp343@gmail.com>
2021-02-13 20:56:55 -0300

    filtrando los productos por categoria

--- products.php
+++ products.php
@@ -7,20 +7,22 @@
         <div>
         <ul>
             <li><a href="#collapseCategoria" role="button" data-toggle="collapse">Categorias</a>
-                <ul class="collapse sublist-menu" id="collapseCategoria">
-                    <li>Auriculares</li>
-                    <li>Teclados</li>
-                    <li>Mouse</li>
-                    <li>Gamepads</li>
+                <ul class="collapse sublist" id="collapseCategoria">
+                    <?php 
+                        foreach($a_categorias as $clave){ ?>
+                        
+                            <li class="activeItem"><a href="products.php?categoria=<?php echo $clave["id_categoria"]; ?>"><?php echo $clave["categoria"]; ?></a></li>
+                            
+                        <?php } ?>
                 </ul>
             </li>
             
             <li><a href="#collapseMarca" role="button" data-toggle="collapse">Marcas</a>
-                <ul class="collapse sublist-menu" id="collapseMarca"></ul>
+                <ul class="collapse sublist" id="collapseMarca"></ul>
             </li>
 
             <li><a href="#collapseCondicion" role="button" data-toggle="collapse">Condición</a>
-                <ul class="collapse sublist-menu" id="collapseCondicion">
+                <ul class="collapse sublist" id="collapseCondicion">
                     <li>Nuevo</li>
                     <li>Destacado</li>
                     <li>Proximamente</li>
@@ -28,7 +30,7 @@
             </li>
 
             <li><a href="#collapsePrecio" role="button" data-toggle="collapse">Precio</a>
-                <ul class="collapse sublist-menu" id="collapsePrecio">
+                <ul class="collapse sublist" id="collapsePrecio">
                     
                 </ul>
             </li>
@@ -39,7 +41,12 @@
     <div class="row row-cols-6">
         <?php 
             for($i = 1; $i <= sizeof($a_productos); $i++){
-                Producto($i, $a_productos);
+                $p_categoria = $a_productos[$i]["id_categoria"];
+                $g_categoria = $_GET["categoria"];
+
+                if($p_categoria == $g_categoria || $g_categoria == 0){
+                    Producto($i, $a_productos);
+                }
             }
 
         ?>
