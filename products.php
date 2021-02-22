8f9a963d094033c5fc4bdb5cab35e76875548324
Gabs343 <gabriellopezp343@gmail.com>
2021-02-21 21:03:22 -0300

    Incorporando el filtro de marcas

--- products.php
+++ products.php
@@ -2,38 +2,13 @@
     require_once("head.php");
 ?>
 
-<section class="container-fluid d-flex mt-5">
-    <div class="filtro">
-        <div>
-        <ul>
-            <li><a href="#collapseCategoria" role="button" data-toggle="collapse">Categorias</a>
-                <ul class="collapse sublist" id="collapseCategoria">
-                    <?php 
-                        FilterList($a_categorias, "categoria", "marca", "condicion");
-                    ?>
-                </ul>
-            </li>
-            
-            <li><a href="#collapseMarca" role="button" data-toggle="collapse">Marcas</a>
-                <ul class="collapse sublist" id="collapseMarca"></ul>
-            </li>
-
-            <li><a href="#collapseCondicion" role="button" data-toggle="collapse">Condición</a>
-                <ul class="collapse sublist" id="collapseCondicion">
-                    <?php 
-                        FilterList($a_condiciones, "condicion", "categoria", "marca");
-                    ?>
-                </ul>
-            </li>
-
-            <li><a href="#collapsePrecio" role="button" data-toggle="collapse">Precio</a>
-                <ul class="collapse sublist" id="collapsePrecio">
-                    
-                </ul>
-            </li>
-        </ul>
-        </div>
-    </div>
+<section class="container-fluid d-flex mt-5"><div class="filtro"><div><ul>
+            <?php 
+                FilterList($a_categorias, "categoria", "marca", "condicion");
+                FilterList($a_marcas, "marca", "categoria", "condicion");
+                FilterList($a_condiciones, "condicion", "categoria", "marca");
+            ?>
+        </ul></div></div>
 
     <div class="row row-cols-6">
         <?php 
@@ -44,13 +19,28 @@
                 $p_condicion = $a_productos[$i]["id_condicion"];
                 $g_condicion = $_GET["condicion"];
 
-                if(($p_categoria == $g_categoria && $p_condicion == $g_condicion) || ($p_categoria == $g_categoria && $g_condicion == 0) || ($p_condicion == $g_condicion && $g_categoria == 0) || ($g_categoria == 0 && $g_condicion == 0)){
+                $p_marca = $a_productos[$i]["id_marca"];
+                $g_marca = $_GET["marca"];
+
+                $allFilters = $p_categoria == $g_categoria && $p_condicion == $g_condicion && $p_marca == $g_marca;
+                $noneFilters = $g_categoria == 0 && $g_condicion == 0 && $g_marca == 0;
+
+                if($allFilters || 
+                    ($p_categoria == $g_categoria && $p_condicion == $g_condicion && $g_marca == 0) ||
+                    ($p_categoria == $g_categoria && $p_marca == $g_marca && $g_condicion == 0) ||
+                    ($p_marca == $g_marca && $p_condicion == $g_condicion && $g_categoria == 0) ||
+
+                    ($p_categoria == $g_categoria && $g_condicion == 0 && $g_marca == 0) ||
+                    ($p_condicion == $g_condicion && $g_categoria == 0 && $g_marca == 0) || 
+                    ($p_marca == $g_marca && $g_categoria == 0 && $g_condicion == 0) ||
+
+                    $noneFilters){
+
                     Producto($i, $a_productos);
                 }
             }
 
-        ?>
-             
+        ?>       
     </div>
 </section>
 
