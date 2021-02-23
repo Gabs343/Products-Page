3f7b6f5722d5c2dcbafa9e00d3cc76fccead9d56
Gabs343 <gabriellopezp343@gmail.com>
2021-02-22 22:28:18 -0300

    funcion que muestra la descripcion de un producto

--- tools.php
+++ tools.php
@@ -220,5 +220,15 @@ function FilterLink($array, $filtro1, $filtro2, $filtro3){
             FilterSublist($clave["id_".$filtro1], $_GET[$filtro1], $linkFilter);
         }
     }
-} 
+}
+
+function TextDescription($string){
+    $array_text = str_split($string);
+
+    for($i = 0; $i < sizeof($array_text); $i++ ){
+
+        echo $array_text[$i];
+        echo $array_text[$i] == "." ? "<br><br>" : "";
+    }
+}
 ?>
