886443bffcbcf8a5195f04f394cf661ce73c5464
Gabs343 <gabriellopezp343@gmail.com>
2021-02-28 22:05:30 -0300

    form de comentarios

--- product-details.php
+++ product-details.php
@@ -22,7 +22,7 @@
 
 </section>
 
-<section class="product-details">
+<section class="product-details container-fluid">
     <h2 class="display-4">Caracteristicas</h2>
     <hr>
     <div class="d-flex container-fluid">
@@ -60,6 +60,43 @@
         </div>
     </div>
 
+</section>
+
+<section class="product-comments">
+    <h2 class="display-4">Comentarios</h2>
+    <hr>
+
+        <form action="" class="container text-center">
+            <div class="form-info d-flex">
+                <div>
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" placeholder="Nombre" id="nombre" name="nombre" required>
+                </div>
+                <div>
+                    <label for="Mail">Email:</label>
+                    <input type="email" placeholder="@example.com" id="Mail" name="id_correo" required>
+                </div>
+                <div>
+                    <label for="Valoracion">Califica el producto:</label>
+                    <?php
+                        for ($i = 1; $i < 6; $i++) { ?>
+                            <input type="radio" name="valoracion" id="Valoracion" value="<?php echo $i ?>" required><?php echo $i ?></input>
+                        <?php } ?>
+                
+                </div>
+            </div>
+            <div class="form-comment">
+                <label for="Comentario">Comentario:</label>
+                <textarea name="comentario" id="Comentario" cols="50" rows="2" required></textarea>
+                
+            </div>
+            <div>
+                <input class="sendForm" type="submit" name="comentar" value="Enviar comentario">
+            </div>
+            
+        </form>
+    
+    <div></div>
 </section>
 
 <?php require_once("footer.php"); ?>
