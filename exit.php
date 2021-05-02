30bc15be408d203165b72b1581dc85a94080a7f6
Gabs343 <gabriellopezp343@gmail.com>
2021-05-02 14:25:11 -0300

    boton para salir

---
+++ exit.php
@@ -0,0 +1,5 @@
+<?php require_once("head.php") ?>
+<form action="index.php" method="POST">
+<input class="sendForm" type="submit" name="sendExit" value="Salir">
+</form>
+<?php require_once("footer.php") ?>
