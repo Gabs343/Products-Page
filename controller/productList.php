f3af96d830b74993bfd0cdd69d5efe550a0487d3
Gabs343 <gabriellopezp343@gmail.com>
2021-05-23 16:54:42 -0300

    asignando la variable productos al view

--- controller/productList.php
+++ controller/productList.php
@@ -9,11 +9,13 @@
             $categorias = $this->modelo->getFiltros("categoria");
             $marcas = $this->modelo->getFiltros("marca");
             $condiciones = $this->modelo->getFiltros("condicion");
+            $productos = $this->modelo->getProductos();
             $this->view->categorias = $categorias;
             $this->view->marcas = $marcas;
             $this->view->condiciones = $condiciones;
+            $this->view->productos = $productos;
             $this->view->render("productList/index");
-        } 
+        }
     }
 
 ?>
