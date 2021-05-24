50e90f4f8059db6d799708be0b560abfc2e80f39
Gabs343 <gabriellopezp343@gmail.com>
2021-05-24 13:41:25 -0300

    funcion para añadir el usuario

--- controller/login.php
+++ controller/login.php
@@ -1,22 +1,37 @@
 <?php 
     class Login extends Controller{
-        function __construct()
+        public function __construct()
         {
             parent::__construct();
         }
 
-        function render(){
+        public function render(){
+            $this->view->subs = $this->isSubmit("sendSubs");
             $this->view->render("login/index");
         }
 
-        function buscarUsuario(){
-            echo "buscar usuario";
+        public function isSubmit($form){
+            if(isset($_POST[$form])){
+                $this->{$form}();
+            }
         }
 
-        function ingresarUsuario(){
-            $this->render();
+        public function sendSubs(){
+            $datos = array(
+                "DNI" => intval($_POST["pk"]),
+                "Nombre" => $_POST["nombre"],
+                "Apellido" => $_POST["apellido"],
+                "Correo" => $_POST["correo"],
+                "Pass" => password_hash($_POST["pwd"], PASSWORD_DEFAULT)
+            );
+            $insertar = $this->modelo->insertSubscriptor($datos);
+            if (!$insertar){
+                $mensaje = "no se pudo insertar el usuario";
+            }else{
+                $mensaje = "usuario insertado con exito"; 
+            }
+            $this->view->mensaje = $mensaje;
         }
-
 
     }
 
