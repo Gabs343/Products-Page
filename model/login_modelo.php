42bd23493197fde1492b0c55ab9f7a2dbf386a02
Gabs343 <gabriellopezp343@gmail.com>
2021-05-24 13:42:47 -0300

    funcion para realizar la query que inserta al usuario

--- model/login_modelo.php
+++ model/login_modelo.php
@@ -1,8 +1,21 @@
 <?php
     class LoginModelo extends Model{
-        function __construct()
+        public function __construct()
         {
             parent::__construct();
+        }
+
+        public function insertSubscriptor($datos){
+            $query = "INSERT INTO cliente (DNI, Nombre, Apellido, Correo, Contraseña) VALUES
+            (:DNI, :Nombre, :Apellido, :Correo, :Pass)";
+            $con = $this->db->connect();
+            $con = $con->prepare($query);
+
+            if($con->execute($datos)){
+                return true;
+            }else{
+                return false;
+            }
         }
     }
 ?>
