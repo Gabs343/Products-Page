96b203b1cc0e534b739d66a4a2e45a091b1c1562
Gabs343 <gabriellopezp343@gmail.com>
2021-05-24 17:59:33 -0300

    metodo para encontrar al usuario

--- model/login_modelo.php
+++ model/login_modelo.php
@@ -17,5 +17,32 @@
                 return false;
             }
         }
+
+        public function findUsuario($datos){
+            $existe = false;
+            try{
+                $query = "SELECT COUNT(*) as existe FROM cliente 
+                        WHERE Correo = '$datos[Correo]'";
+                $con = $this->db->connect();
+                $con = $con->query($query)->fetch();
+                if(intval($con["existe"])){
+                    $query = "SELECT Nombre, Correo, Contraseña FROM cliente WHERE Correo = '$datos[Correo]'";
+                    $con = $this->db->connect();
+                    $usuario = $con->query($query)->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($usuario as $clave){
+                        if(password_verify($datos["Pass"], $clave["Contraseña"])){
+                            $existe = true;
+                            return $existe;
+                        }else{
+                            return $existe;
+                        }
+                    }
+                }else{
+                    return $existe;
+                }
+            }catch(PDOException $e){
+                return $existe;
+            }
+        }
     }
 ?>
