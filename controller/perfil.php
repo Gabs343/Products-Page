56cb5aada8bba070008c96cbab86599097c7e3b2
Gabs343 <gabriellopezp343@gmail.com>
2021-06-05 15:27:59 -0300

    funcion de render

--- controller/perfil.php
+++ controller/perfil.php
@@ -1,11 +1,13 @@
 <?php 
     class Perfil extends Controller{
-        function __construct()
+        public function __construct()
         {
             parent::__construct();
         }
 
-        function render(){
+        public function render(){
+            $info = $this->modelo->getInfo();
+            $this->view->info = $info;
             $this->view->render("perfil/index");
         }
     }
