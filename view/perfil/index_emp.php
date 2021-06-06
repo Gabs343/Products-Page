0915895471ccb82675701c77504441da0f9891d4
Gabs343 <gabriellopezp343@gmail.com>
2021-06-06 19:01:26 -0300

    index del perfil para los empleados

---
+++ view/perfil/index_emp.php
@@ -0,0 +1,7 @@
+<?php require "view/header.php"; ?>
+<div class="salir-perfil">
+    <form action="main" method="POST">
+        <input class="sendForm" type="submit" name="sendExit" value="Salir">
+    </form>
+</div>
+<?php require "view/footer.php"; ?>
