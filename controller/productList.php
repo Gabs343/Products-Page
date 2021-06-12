aa5046c3d526e9a1d4a3a3090ea99c754736010b
Gabs343 <gabriellopezp343@gmail.com>
2021-06-12 14:09:12 -0300

    llamando a los filtros y productos

--- controller/productList.php
+++ controller/productList.php
@@ -17,7 +17,15 @@
             $this->view->render("productList/index");
         }
 
-        public function renderForEmpleados(){
+        public function renderForEmpleados(){ //unir con la funcion de render
+            $categorias = $this->modelo->getFiltros("categoria");
+            $marcas = $this->modelo->getFiltros("marca");
+            $condiciones = $this->modelo->getFiltros("condicion");
+            $productos = $this->modelo->getProductos();
+            $this->view->categorias = $categorias;
+            $this->view->marcas = $marcas;
+            $this->view->condiciones = $condiciones;
+            $this->view->productos = $productos;
             $this->view->render("productList/index_emp");
         }
     }
