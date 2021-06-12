4594541efe6e5ac700f6a3244f75341ac5caaa44
Gabs343 <gabriellopezp343@gmail.com>
2021-06-12 19:27:45 -0300

    funcion que indica si se muestra o no el producto y funcion de editar

--- controller/productList.php
+++ controller/productList.php
@@ -26,8 +26,42 @@
             $this->view->marcas = $marcas;
             $this->view->condiciones = $condiciones;
             $this->view->productos = $productos;
+            $this->view->mostrar = $this->isSubmit("mostrarProducto");
+            $this->view->editar = $this->isSubmit("editarProducto");
             $this->view->render("productList/index_emp");
         }
+
+        public function isSubmit($form){
+            if(isset($_POST[$form])){
+                $this->{$form}();
+            }
+        }
+
+        public function mostrarProducto(){
+            
+            if($_POST["mostrarProducto"] == "Activar"){
+                $estado = array(
+                    "ID" => intval($_POST["ID"]),
+                    "Activo" => 1 
+                );
+            }else if($_POST["mostrarProducto"] == "Desactivar"){
+                $estado = array(
+                    "ID" => intval($_POST["ID"]),
+                    "Activo" => 0 
+                );
+            }
+            $exito = $this->modelo->actualizarProducto($estado);
+
+            if($exito){
+                //echo "EXITO";
+                echo "<meta http-equiv='refresh' content='0'>";
+            }
+        }
+
+        public function editarProducto(){
+            echo "editar";
+        }
+
     }
 
 ?>
