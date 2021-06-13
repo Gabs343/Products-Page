d909e3961866c7fc1407046146a64bec60ba245a
Gabs343 <gabriellopezp343@gmail.com>
2021-06-12 23:59:01 -0300

    form para editar los productos

---
+++ view/productDetails/index_emp.php
@@ -0,0 +1,59 @@
+<?php require "view/header.php"; ?>
+<form action="<?php $_PHP_SELF;?>" method="POST">
+
+<section class="d-sm-flex product-info">
+    <div>
+        <input class="display-3 text-center" type="text" placeholder="<?php echo $this->producto->nombre; ?>">
+        <hr class="linea">
+        <p><textarea name="" id="" cols="80" rows="10" placeholder="<?php echo $this->producto->descripcion;?>"></textarea></p>
+    </div>
+
+    <div class="product-img">
+        <img src="<?php echo $this->producto->imagen; ?>" alt="" class="d-block w-100">
+        <div class="shop-buttons mt-5">
+            <h3><input type="number" placeholder="$ <?php echo $this->producto->precio; ?>"></h3>
+            <div class="ml-5">
+                <input type="submit" value="Confirmar">
+                <a href="productList?categoria=0&marca=0&condicion=0&orden=0">Cancelar</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="product-details container-fluid">
+    <h2 class="display-4">Características</h2>
+    <hr class="linea">
+
+    
+</section>
+</form>
+
+<section class="product-comments pb-5">
+    <div class="container">
+        <h2 class="display-4 comen-m">Comentarios</h2>
+        <hr class="linea">
+        <ul>
+            <?php foreach($this->comentarios as $clave){ ?>
+                <li class="comen-muestra">
+                    <?php
+                        foreach($clave as $subclave => $subvalor){
+                            if($subclave == "Valoracion"){
+                            echo $subclave, ": ";
+                            for($i = 0; $i < $subvalor; $i++){?>
+                                <i class="fas fa-star"></i>
+                            <?php } ?>
+                            <br>
+                            <?php }else{
+                                echo $subclave, ": ", $subvalor, "<br>";
+                            }
+                        } ?>
+                    <form action="<?php $_PHP_SELF;?>" method="POST">
+                        <input type="submit" value="">
+                    </form>
+                </li>
+            <?php  } ?>
+        </ul>
+    </div>
+</section>
+
+<?php require "view/footer.php"; ?>
